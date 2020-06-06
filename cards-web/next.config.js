f58cadc1ed6e52aa9e7272c8acbba9ca8c2605e2
Adam <hello@allotrope.io>
2020-06-06 09:41:28 -0700

    disable PWA in development, not possible to remove duplicate GenerateSW calls

--- cards-web/next.config.js
+++ cards-web/next.config.js
@@ -3,6 +3,7 @@ const withPWA = require('next-pwa');
 module.exports = withPWA({
   // other next config
   pwa: {
-      dest: 'public'
+      dest: 'public',
+      disable: process.env.NODE_ENV !== 'production'
   }
 })
